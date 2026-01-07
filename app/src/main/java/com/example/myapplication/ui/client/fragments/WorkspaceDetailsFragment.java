afb2401ea9351a2a4238d2b5eea80753de56ca1a
YassineRH <my.reghioui@gmail.com>
2026-01-07 16:24:26 +0100

    Fixed fragment folder organizing

--- app/src/main/java/com/example/myapplication/ui/client/fragments/WorkspaceDetailsFragment.java
+++ app/src/main/java/com/example/myapplication/ui/client/fragments/WorkspaceDetailsFragment.java
@@ -1,4 +1,4 @@
-package com.example.myapplication.ui.client;
+package com.example.myapplication.ui.client.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,6 +16,7 @@ import androidx.fragment.app.Fragment;
 import com.example.myapplication.R;
 import com.example.myapplication.model.Review;
 import com.example.myapplication.model.Workspace;
+import com.example.myapplication.ui.client.BookingActivity;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
